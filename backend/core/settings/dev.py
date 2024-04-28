0bcd6461ddfd374af03d54cbab7385aeb2e02b6f
serhatuluc <ulucs15@itu.edu.tr>
2024-04-28 21:55:27 +0300

    backend is created

---
+++ backend/core/settings/dev.py
@@ -0,0 +1,16 @@
+from .base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-*um&iim#3(gig!r7x3m+h8qhy@@+r96xpji3kin&ifcew)awm4'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
