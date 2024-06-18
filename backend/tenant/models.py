3dbbd55de4c7c44dc198905806b11af3ce75cd46
serhatuluc <ulucs15@itu.edu.tr>
2024-06-18 14:27:44 +0300

    Info page is implemented

--- backend/tenant/models.py
+++ backend/tenant/models.py
@@ -17,9 +17,10 @@ class Product(models.Model):
         on_delete=models.CASCADE,
         related_name="products",
     )
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to="media")
+    image = models.ImageField(upload_to="media", default='')
     sale_price = models.DecimalField(
         max_digits=10,
         decimal_places=2,
@@ -28,7 +29,7 @@ class Product(models.Model):
     )
     is_on_sale = models.BooleanField(default=False)
     expire_date = models.DateField(default=date.today)
-    contents = models.TextField()
+    contents = models.TextField(default='')
 
     def __str__(self) -> str:
         return self.name
@@ -40,3 +41,23 @@ class Product(models.Model):
             w, h = get_image_dimensions(self.image)
             if w != 270 and h != 370:
                 raise ValidationError("Fotoğraf 270 x 370 boyutunda olmalıdır!")
+
+
+class ProductSpesifications(models.Model):
+    product = models.OneToOneField(Product, on_delete=models.CASCADE)
+    key = models.CharField(max_length=20)
+    value = models.CharField(max_length=4000)
+
+    def __str__(self):
+        return f"{self.product.name}'s settings"
+
+
+class ProductOrderingInformations(models.Model):
+    product = models.OneToOneField(Product, on_delete=models.CASCADE)
+    material = models.CharField(max_length=20)
+    description = models.CharField(max_length=4000)
+    packaging = models.CharField(max_length=4000)
+
+
+    def __str__(self):
+        return f"{self.product.name}'s settings"
--- backend/tenant/views/product_views.py
+++ backend/tenant/views/product_views.py
@@ -1,12 +1,45 @@
-from tenant.models import Product
+from tenant.models import Product, ProductOrderingInformations, ProductSpesifications
 from tenant.utilities import get_tenant
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from tenant.serializers import ProductSerializer
+from tenant.serializers import ProductOrderingInformationsSerializer, ProductSerializer, ProductSpesificationsSerializer
 
 @api_view(["GET"])
 def get_products(request) -> Response:
     tenant = get_tenant(request)
     products = Product.objects.filter(tenant=tenant)
     serializer = ProductSerializer(products, many=True, context={"request": request})
+    return Response(serializer.data)
+
+@api_view(["GET"])
+def get_product(request,productId) -> Response:
+    tenant = get_tenant(request)
+    product = Product.objects.filter(id=productId,tenant=tenant).first()
+    serializer = ProductSerializer(product, many=False, context={"request": request})
+    return Response(serializer.data)
+
+
+@api_view(["GET"])
+def get_product_spesifications(request,productId) -> Response:
+    try:
+        # Assuming productId is the actual ID of the product
+        product = Product.objects.get(id=productId)
+    except Product.DoesNotExist:
+        return Response({"error": "Product not found"}, status=404)
+
+    productSpesifications = ProductSpesifications.objects.filter(product = product)
+    serializer = ProductSpesificationsSerializer(productSpesifications, many=True, context={"request": productId})
+    return Response(serializer.data)
+
+
+@api_view(["GET"])
+def get_product_ordering_informations(request,productId) -> Response:
+    try:
+        # Assuming productId is the actual ID of the product
+        product = Product.objects.get(id=productId)
+    except Product.DoesNotExist:
+        return Response({"error": "Product not found"}, status=404)
+
+    productOrderingInformations = ProductOrderingInformations.objects.filter(product = product)
+    serializer = ProductOrderingInformationsSerializer(productOrderingInformations, many=True, context={"request": productId})
     return Response(serializer.data)
